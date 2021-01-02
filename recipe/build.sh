136fc761f2728e2fc9f31bb887f3992e337733d1
shadow_walker <shadowwalkersb@gmail.com>
2021-01-01 18:23:48 -0600

    gnuconfig -> libtool

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Get an updated config.sub and config.guess
-cp $BUILD_PREFIX/share/gnuconfig/config.* ./nspr/build/autoconf
+cp $BUILD_PREFIX/share/libtool/build-aux/config.* .
 
 cd nspr
 
